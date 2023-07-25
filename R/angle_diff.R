7c97141db80dcb691f0d5a6855f3cee8ee3c0a96
Guillermo Basulto <guillermobasulto@gmail.com>
2023-07-25 08:44:03 -0500

    Add new functions.

---
+++ R/angle_diff.R
@@ -0,0 +1,23 @@
+
+#' Angle Difference
+#'
+#' Compute the angle difference. The Output are values between 0 and 180 degrees.
+#'
+#' @param theta Vector of angles of size n.
+#'
+#' @return A vector of size n whose first entry is NA, the second is the difference between the 2nd and 1st, and so on.
+#' @export
+#'
+#' @examples
+#' angle_diff(c(330, 15, 60, 15))
+angle_diff <- function(theta) {
+
+  len <- length(theta)
+
+  ## Define angles and convert them to radians
+  th1 <- c(NA, theta[-len]) / 360 * 2 * pi
+  th2 <- theta / 360 * 2 * pi
+
+  acos(cos(th1 - th2)) / 2 / pi * 360
+}
+
--- R/cross_prod_sign.R
+++ R/cross_prod_sign.R
@@ -1,4 +1,31 @@
 
+#' Cross Product Sign
+#'
+#' Calculate the sign of the cross product.
+#'
+#' Negative values are associated with right turns while positive values with left turns.
+#'
+#' @param lat Vector of latitudes of size n.
+#' @param lon Vector of longitudes of size n.
+#'
+#' @return Vector of cross product signs of size n. The first element and last elements are NA.
+#' @export
+#'
+#' @examples
+#' cities <-
+#' tibble::tribble(
+#'   ~city, ~latitude, ~longitude,
+#'   "Las Vegas", 36.1699, -115.1398,
+#'   "San Diego", 32.7157, -117.1611,
+#'   "Los Angeles", 34.0522, -118.2437,
+#'   "San Francisco", 37.7749, -122.4194,
+#'   "Portland", 45.5122, -122.6587,
+#'   "Seattle", 47.6062, -122.3321,
+#'   "Vancouver", 49.2827, -123.1207,
+#'   "Anchorage", 61.2181, -149.9003
+#' )
+#'
+#' with(cities, cross_prod_sign(latitude, longitude))
 cross_prod_sign <- function(lat, lon) {
   len <- length(lat)
 
